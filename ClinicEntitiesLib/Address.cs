68b59b345054d0d6136102648e383416933269c6
vladar21 <vlad.rastvorov@aol.com>
2020-08-12 11:27:17 +0300

    start

---
+++ ClinicEntitiesLib/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace Clinic.VR
+{
+    class Address
+    {
+        public int AddressID { get; set; } //  id int
+        public int ZipCode { get; set; } //  zipcode varchar(255)
+        public string Country { get; set; } //  country varchar(255)
+        public string Region { get; set; } //  region varchar(255)
+        public string Locality { get; set; } //  locality varchar(255)
+        public string Street { get; set; } //  street varchar(255)
+        public string House { get; set; } //  house varchar(255)
+        public string Apartment { get; set; } //  apartment varchar(255)
+
+    }
+}
